38f37186cf86d8415fe019d174977b410e6b0d31
zeenontop <97391523+zeenontop@users.noreply.github.com>
2025-01-30 02:45:09 +0200

    Update index_scripts.js

--- index_scripts.js
+++ index_scripts.js
@@ -1,1 +1,16 @@
-// External JS: http://217.72.204.169/index.js
+// Get the elements
+const downloadButton = document.querySelector('.download-button');
+const heroVideo = document.querySelector('.hero video');
+
+
+heroVideo.style.filter = 'blur(4px)';
+
+
+// Add event listeners for mouseenter and mouseleave
+downloadButton.addEventListener('mouseenter', () => {
+    heroVideo.style.filter = 'blur(10px)';
+});
+
+downloadButton.addEventListener('mouseleave', () => {
+    heroVideo.style.filter = 'blur(4px)';
+});
